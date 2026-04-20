3fd37c9c2d06deaa549318946c038225750dd101
Puja Rasaily <pujarasaily1998@gmail.com>
2026-04-20 10:13:12 +0530

    made code changes

--- IntroductionSelenum/src/Selenium/SelIntroduction.java
+++ IntroductionSelenum/src/Selenium/SelIntroduction.java
@@ -10,6 +10,10 @@ public class SelIntroduction {
 		//ChromeDriver driver=new  ChromeDriver();-->will not work on other browser
 		System.setProperty("webdriver.chrom.driver","C:\\Users\\LENOVO\\Downloads\\chromedriver.exe");//SeleniumManager will not be in active state
 		WebDriver driver=new ChromeDriver();
+		
+		
+		
+	System.out.println("Updated code for Git");
 		}
 
 }
